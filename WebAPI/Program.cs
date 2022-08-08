f1a049c67d2bfa67a7a64a4f35ae7f6d690cbdc5
tronbodya <tronbodya@gmail.com>
2022-08-08 23:33:45 +0300

    setup

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
 
 app.Run();
